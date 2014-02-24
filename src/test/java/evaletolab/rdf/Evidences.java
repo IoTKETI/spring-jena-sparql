b51daa3833641bb1613055f4ab004759fa40b6ed
olivier <olivier.evalet@isb-sib.ch>
2014-02-24 16:38:58 +0100

    more sparql test

--- src/test/java/evaletolab/rdf/Evidences.java
+++ src/test/java/evaletolab/rdf/Evidences.java
@@ -19,6 +19,8 @@ import com.hp.hpl.jena.query.ResultSetFormatter;
 import com.hp.hpl.jena.rdf.model.InfModel;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.reasoner.Reasoner;
+import com.hp.hpl.jena.reasoner.ReasonerRegistry;
 
 import evaletolab.config.WebConfig;
 
@@ -37,21 +39,28 @@ import evaletolab.config.WebConfig;
 @ContextConfiguration(classes = WebConfig.class)
 public class Evidences {
 	
-	Model m;
+	Model m, schema;
 	InfModel rdfs;
 	
 	@Before
 	public void setup() {
 		m=ModelFactory.createDefaultModel();
-		m.read("evidence.ttl").read("terminology-disease.ttl").read("publication.ttl").read("owl/np.ttl");
-		rdfs= ModelFactory.createRDFSModel(m);
+		schema=ModelFactory.createDefaultModel();
+		m.read("evidence.ttl").read("terminology-disease.ttl").read("publication.ttl");
+        schema = schema.read("owl/np.ttl");
+
+//        Reasoner reasoner = ReasonerRegistry.getOWLReasoner();
+//        reasoner = reasoner.bindSchema(schema);
+//        rdfs = ModelFactory.createInfModel(reasoner, m);
+        
+		rdfs= ModelFactory.createRDFSModel(schema, m);
 
 
 	}
 	
 	/**
-	 * Q53	which are involved in cell adhesion according to GO with 
-	 *      an evidence not IAE and not ISS 
+	 * PREPARE QUERY Q53
+	 * 	which are involved in cell adhesion according to GO  
 	 *      - Cell adhesion [GO:0007155] 
 	 */
 	@Test
@@ -62,13 +71,18 @@ public class Evidences {
 				 "PREFIX owl: <http://www.w3.org/2002/07/owl#> "+
 				 "PREFIX : <http://nextprot.org/rdf#> " +
 				 "PREFIX term: <http://nextprot.org/rdf/terminology/> " +
-				 "SELECT ?entry WHERE { " +
-				 "  ?entry :isoform/:involved/:in term:GO:0007155"+
+				 "SELECT ?involvedInGO0007155 WHERE { " +
+				 "  ?involvedInGO0007155 :isoform/:function/:in term:GO:0007155"+
 				 "}";	
 		Query query = QueryFactory.create(q);
         QueryExecution qe = QueryExecutionFactory.create(query,rdfs);
+                
+        
         ResultSetFormatter.out(System.out, qe.execSelect(), query);
-	}		
+	}	
+	
+
+
 	
 	/**
 	 * Q53	which are involved in cell adhesion according to GO with 
@@ -83,10 +97,11 @@ public class Evidences {
 				 "PREFIX owl: <http://www.w3.org/2002/07/owl#> "+
 				 "PREFIX : <http://nextprot.org/rdf#> " +
 				 "PREFIX term: <http://nextprot.org/rdf/terminology/> " +
-				 "SELECT ?entry WHERE { " +
-				 "  ?entry  :isoform/:involved ?statement;:in term:GO:0007155"+
+				 "SELECT ?involvedInGO0007155_WithEvidence_NotIEA_And_NotISS WHERE { " +
+				 "  ?involvedInGO0007155_WithEvidence_NotIEA_And_NotISS  :isoform/:function ?statement." +
+				 "  ?statement :in term:GO:0007155."+
 				 "  FILTER NOT EXISTS { " +
-				 "    {?statement :evidence/rdfs:type :IAE }UNION{?statement :evidence/rdfs:type :ISS }"+
+				 "    {?statement :evidence/rdf:type :IAE }UNION{?statement :evidence/rdf:type :ISS }"+
 				 "  } "+
 				 "}";	
 		Query query = QueryFactory.create(q);
@@ -94,57 +109,9 @@ public class Evidences {
         ResultSetFormatter.out(System.out, qe.execSelect(), query);
 	}	
 
-	@Test
-	public void involvedInGO0007155_WithEvidence_NotIEA_And_NotISS_1(){
-		// query
-		String q="PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
-				 "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"+
-				 "PREFIX owl: <http://www.w3.org/2002/07/owl#> "+
-				 "PREFIX : <http://nextprot.org/rdf#> " +
-				 "PREFIX term: <http://nextprot.org/rdf/terminology/> " +
-				 "SELECT ?entry WHERE { " +
-				 "  ?entry  :isoform/:involved ?statement;:in term:GO:0007155"+
-				 "  FILTER NOT EXISTS { ?statement :evidence/rdfs:type/owl:unionOf (:IAE :ISS)} "+
-				 "}";	
-		Query query = QueryFactory.create(q);
-        QueryExecution qe = QueryExecutionFactory.create(query,rdfs);
-        ResultSetFormatter.out(System.out, qe.execSelect(), query);
-	}	
-
-	@Test
-	public void involvedInGO0007155_WithEvidence_NotIEA_And_NotISS_2(){
-		// query
-		String q="PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
-				 "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"+
-				 "PREFIX owl: <http://www.w3.org/2002/07/owl#> "+
-				 "PREFIX : <http://nextprot.org/rdf#> " +
-				 "PREFIX term: <http://nextprot.org/rdf/terminology/> " +
-				 "SELECT ?entry WHERE { " +
-				 "  ?entry  :isoform/:involved ?statement;:in term:GO:0007155."+
-				 "  ?statement :evidence/rdfs:type/owl:differentFrom :IAE;owl:differentFrom :ISS."+
-				 "}";	
-		Query query = QueryFactory.create(q);
-        QueryExecution qe = QueryExecutionFactory.create(query,rdfs);
-        ResultSetFormatter.out(System.out, qe.execSelect(), query);
-	}	
-
-	@Test
-	public void involvedInGO0007155_WithEvidence_NotIEA_And_NotISS_3(){
-		// query
-		String q="PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
-				 "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"+
-				 "PREFIX owl: <http://www.w3.org/2002/07/owl#> "+
-				 "PREFIX : <http://nextprot.org/rdf#> " +
-				 "PREFIX term: <http://nextprot.org/rdf/terminology/> " +
-				 "SELECT ?entry WHERE { " +
-				 "  ?entry  :isoform/:involved ?statement;:in term:GO:0007155."+
-				 "  ?statement :evidence/rdfs:type/owl:complementOf :IAE;owl:complementOf :ISS. "+
-				 "}";	
-		Query query = QueryFactory.create(q);
-        QueryExecution qe = QueryExecutionFactory.create(query,rdfs);
-        ResultSetFormatter.out(System.out, qe.execSelect(), query);
-	}	
 	
+
+
 	/**
 	 * Q57	which are located in mitochondrion with an evidence other 
 	 *      than HPA and DKFZ-GFP 
