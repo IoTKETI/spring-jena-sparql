97551c61f65a50927dee2258048c3d9a7aec047c
olivier <olivier.evalet@isb-sib.ch>
2014-03-11 17:43:38 +0100

    more test for expression

--- src/test/java/evaletolab/rdf/Expression.java
+++ src/test/java/evaletolab/rdf/Expression.java
@@ -21,12 +21,12 @@ import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 
 import evaletolab.config.WebConfig;
+import evaletolab.tool.FileUtil;
 
 /**
  * Use case for expression queries
  * -  Proteins that are not highly expressed in liver at embrion stage
  * -  Proteins that are expressed in liver and involved in transport
- * -  Proteins that are expressed   in liver and involved in transport 
  * -  Proteins >=1000 amino acids and located in nucleus and expression in nervous system
  * -  Proteins highly expressed at IHC level in heart
  * -  Proteins highly expressed in* brain but not expressed in* testis
@@ -50,63 +50,33 @@ public class Expression {
 	@Before
 	public void setup() {
 		m=ModelFactory.createDefaultModel();
-		m.read("expression-heavy.ttl").read("owl/np.ttl");
+		m.read("owl/np.ttl");
 		rdfs= ModelFactory.createRDFSModel(m);
 		
 		
-		//
-		// load data
-		String q="PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
-				 "PREFIX : <http://np.org/rdf#> " +
-				 "SELECT * WHERE { " +
-				 "  ?tissue a :Tissue . " +
-				 "}";		
-		Query query = QueryFactory.create(q);
-        QueryExecution qe = QueryExecutionFactory.create(query,rdfs);
-        qe.execSelect();	
+
 	}
 	
-	@Test
-	//https://code.google.com/p/project-knowledgetv/source/browse/trunk/knowledge-tv/knowledge-tv-sqtv-lib/src/test/java/br/ufpb/di/knowledgetv/sqtv/tests/JenaTest.java?r=24
-	public void listIsoforms(){
-		String q="PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
-				 "PREFIX : <http://np.org/rdf#> " +
-				 "SELECT * WHERE { " +
-				 "  ?isoform a :Isoform . " +
-				 "}";
-		
-		
-		
-		long start=System.currentTimeMillis();
-		Query query = QueryFactory.create(q);
-        QueryExecution qe = QueryExecutionFactory.create(query,rdfs);
-        ResultSet rs = qe.execSelect();
-        System.out.println("isoforms: "+(System.currentTimeMillis()-start)+" ms");
-        
-        ResultSetFormatter.out(System.out, rs, query);
-		assertEquals(rs.getRowNumber(), 122);
-//        while(rs.hasNext()){
-//        	 QuerySolution row= rs.next();
-//             String value= row.getLiteral("name").toString();        }
-	}
+
 	
 	/**
-	 * Proteins that are not highly expressed in liver at embrion stage
+	 * Proteins that are not highly expressed in liver at embrion stage (count=11'272)
+	 * --> hierarchical Liver (TS-0564) with 17848 entries
+	 *     NX_P61604,NX_Q15029,NX_Q07973
+	 * --> not highly expressed =>  :expressionLevel/owl:disjointWith :High.
+	 * @throws Exception 
+	 * 
 	 */
 	@Test
-	public void notHighlyExpressedAtEmbrionStage(){
+	public void notHighlyExpressedAtEmbrionStage() throws Exception{
 		//
 		// specific query
-		String q="PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
-				 "PREFIX : <http://np.org/rdf#> " +
-				 "PREFIX tissue: <http://np.org/rdf/terminology/> "+
-				 "SELECT * WHERE { " +
-				 "  ?isoform :notHighlyExpressed/:in tissue:Bile%20duct. " +
-				 "}";
+		String q=FileUtil.getResourceAsString("sparql/notHighlyExpressedAtEmbrionStage.sparql");
 
 
 		//
 		// execute query
+		System.out.println(q);
 		Query query = QueryFactory.create(q);
 		long start=System.currentTimeMillis();
         QueryExecution qe = QueryExecutionFactory.create(query,rdfs);
@@ -120,10 +90,16 @@ public class Expression {
 	
 	/**
 	 * Q11, Proteins that are expressed in liver and involved in transport
+	 * --> hierarchical Liver (TS-0564) with 17848 entries
+	 *     NX_P61604,NX_Q15029,NX_Q07973
+	 * --> :classifiedWith Transport (KW-0813) with 1859 entries
+	 *     NX_Q9H0U6, NX_P08195, NX_P46098 
+	 * @throws Exception 
 	 */
 	@Test
-	public void expressedInLiverAndInvolvedInTransport(){
-
+	public void expressedInLiverAndInvolvedInTransport() throws Exception{
+		String q=FileUtil.getResourceAsString("sparql/expressedInLiverAndInvolvedInTransport.sparql");
+		System.out.println(q);
 	}
 	
 	/**
@@ -137,36 +113,46 @@ public class Expression {
 	
 	/**
 	 * Q4, Proteins highly expressed in brain but not expressed in testis
-	 *  --> hierarchical Terms for Testis and Brain
+	 *  --> hierarchical Terms for Testis (TS-1030)
+	 *  --> Brain 18202 entries, NX_P61604, NX_Q15029, NX_Q07973
+	 *  --> Testis TS-1030  18117 entries
+	 * @throws Exception 
 	 */
 	@Test
-	public void highlyExpressedInBrainButNotInTestis(){
-
+	public void highlyExpressedInBrainButNotInTestis() throws Exception{
+		String q=FileUtil.getResourceAsString("sparql/highlyExpressedInBrainButNotInTestis.sparql");
+		System.out.println(q);
 	}			
 	
 	/**
 	 * Q50, Proteins which are expressed in brain according to IHC 
 	 *      but not expressed in brain according to microarray
+	 * @throws Exception 
 	 */
 	@Test
-	public void expressedInBrainAccordingIHC(){
-
+	public void expressedInBrainAccordingIHCButNotExpressedInBrainAccordingMicroarray() throws Exception{
+		String q=FileUtil.getResourceAsString("sparql/highlyExpressedInBrainButNotInTestis.sparql");
+		System.out.println(q);
 	}			
 	
 	/**
 	 * Q83, Proteins whose genes are on chromosome N that are expressed only a single tissue/organ
+	 * @throws Exception 
 	 */
 	@Test
-	public void expressedOnASingleTissue(){
-
+	public void expressedOnASingleTissue() throws Exception{
+		String q=FileUtil.getResourceAsString("sparql/expressedOnASingleTissue.sparql");
+		System.out.println(q);
 	}		
 	
 	/**
 	 * Q77, Proteins which are expressed in liver according to IHC data but not found in HUPO liver proteome set
+	 * @throws Exception 
 	 */
 	@Test
-	public void expressedInLiverAccordingIHC(){
-
+	public void expressedInLiverAccordingIHCButNotInHUPOLiverProteom() throws Exception{
+		String q=FileUtil.getResourceAsString("sparql/expressedInLiverAccordingIHCButNotInHUPOLiverProteom.sparql");
+		System.out.println(q);
 	}	
 	
 	/**
@@ -174,7 +160,7 @@ public class Expression {
 	 *  --> hierarchical terms for Brain
 	 */
 	@Test
-	public void expresssedInBrain(){
+	public void PDZdomainthatInteractWithProteinExpresssedInBrain(){
 
 	}	
 	
