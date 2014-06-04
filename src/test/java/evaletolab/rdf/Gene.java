ac6ed77b9875cbe07d921d02d3e27dab72d3c01f
Daniel <ddtxra@gmail.com>
2014-06-04 14:54:00 +0200

    adding q058 (very long)

--- src/test/java/evaletolab/rdf/Gene.java
+++ src/test/java/evaletolab/rdf/Gene.java
@@ -12,23 +12,25 @@ import evaletolab.controller.TripleStoreBaseTest;
  */
 public class Gene extends TripleStoreBaseTest {
 
-	/**
-	 * Q55 which have genes of length >=10000 bp
-	 * 
-	 * @throws Exception
-	 */
 	@Test
 	public void Q55_wich_have_genes_of_length_bigger_than_10000BP() {
 		testSparql("Q055.sparql");
 	}
 
-	/**
-	 * Q58 which are located on the genome next to a protein which is involved in spermatogenesis
-	 * 
-	 * @throws Exception
-	 */
 	@Test
 	public void Q58_which_are_located_on_the_genome_next_to_a_protein_which_is_involved_in_spermatogenesis_right() {
 		testSparql("Q058-right.sparql");
 	}
+	
+	
+	@Test
+	public void Q58_which_are_located_on_the_genome_next_to_a_protein_which_is_involved_in_spermatogenesis_left() {
+		testSparql("Q058-left.sparql");
+	}
+
+	@Test
+	public void Q58_which_are_located_on_the_genome_next_to_a_protein_which_is_involved_in_spermatogenesis() {
+		testSparql("Q058.sparql");
+	}
+
 }
