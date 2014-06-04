17ef388fb56680565f2223e1b65190e9028a85d0
Pierre-Andre Michel <pierre-andre.michel@isb-sib.ch>
2014-06-04 13:02:56 +0200

    added Q95 in Federated.java test

---
+++ src/test/java/evaletolab/rdf/Federated.java
@@ -0,0 +1,24 @@
+package evaletolab.rdf;
+
+import org.junit.Test;
+
+import evaletolab.controller.TripleStoreBaseTest;
+
+/**
+ * Use case for federated queries
+ * Q95 which are targets of antibiotics (federated query with drugbank)
+ * 
+ * @author pam
+ */
+public class Federated extends TripleStoreBaseTest{
+	
+	
+	/**
+	 * Q55 which have genes of length >=10000 bp
+	 * @throws Exception 
+	 */
+	@Test
+	public void Q95_targetsForANtibiotics() {
+		testSparql("Q95.sparql");
+	}	
+}
