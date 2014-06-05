93f5861930babfd353114acccd2b839d2169f039
Pierre-Andre Michel <pierre-andre.michel@isb-sib.ch>
2014-06-05 09:33:19 +0200

    Added Q107 and Q4

---
+++ src/test/java/evaletolab/rdf/Slow.java
@@ -0,0 +1,23 @@
+package evaletolab.rdf;
+
+import org.junit.Test;
+
+import evaletolab.controller.TripleStoreBaseTest;
+
+/**
+ * Use case for slow queries
+ * 
+ * @author pam
+ */
+public class Slow extends TripleStoreBaseTest{
+	
+	
+	/**
+	 * Q4-faster high in brain not in testis
+	 * @throws Exception 
+	 */
+	@Test
+	public void Q4faster_HighInBrain_NotInTestis() {
+		testSparql("Q4-faster.sparql");
+	}	
+}
