2e0a0425fd1c5ea3834c4b4df6e223e398e3acfa
evaletolab <olivier.evalet@isb-sib.ch>
2014-09-04 17:39:54 +0200

    fix query 41

--- src/test/java/evaletolab/rdf/Slow.java
+++ src/test/java/evaletolab/rdf/Slow.java
@@ -16,7 +16,7 @@ public class Slow extends TripleStoreBaseTest{
 	 * Q4-faster high in brain not in testis
 	 * @throws Exception 
 	 */
-	@Test
+	//@Test
 	public void Q4faster_HighInBrain_NotInTestis() {
 		testSparql("Q4-faster.sparql");
 	}	
